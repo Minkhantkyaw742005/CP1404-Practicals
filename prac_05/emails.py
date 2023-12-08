92da63233d639bf2fd60a19d9d3a9a3f65333532
Minkhantkyaw742005 <minkhant.kyaw@my.jcu.eduau>
2023-12-08 21:51:36 +0800

    File for practical 5

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,9 +1,28 @@
-email_to_name = {}
-email = input("Email: ")
+def main():
+    """Create a dictionary of emails-to-names."""
+    email_to_name = {}
+    user_email = input("Email: ")
 
-while email != "":
-    prefix = email.split("@")[0]
-    name_parts = prefix.split(".")
-    name = " ".join(name_parts).title()
-    name_confirm = input(f"Is this your name {name}? (Y/n)")
-    if name_confirm != "Y" and
+    while user_email != "":
+        user_name = get_name_from_email(user_email)
+        confirmation = input(f"Is your name {user_name}? (Y/n) ")
+
+        if confirmation.upper() != "Y" and confirmation != "":
+            user_name = input("Name: ")
+
+        email_to_name[user_email] = user_name
+        user_email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    """Extract an expected name from the email address."""
+    email_prefix = email.split('@')[0]
+    name_parts = email_prefix.split('.')
+    user_name = " ".join(name_parts).title()
+    return user_name
+
+
+main()
